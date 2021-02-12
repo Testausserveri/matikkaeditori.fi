4fea6c7995099f68e2b7313cce763c8a62297aa1
Esinko <34211401+Esinko@users.noreply.github.com>
2021-02-12 09:07:17 +0200

    fix!: Move app code to index.js

--- src/App.js
+++ src/App.js
@@ -1,88 +1,12 @@
 /* eslint-disable react/react-in-jsx-scope */
-// Import static components
-import * as C from "./js/console.js"
-import * as c from "./js/worker-components/compatibility.js"
-import * as Workers from "./js/workers.js"
-// eslint-disable-next-line no-unused-vars
-import * as uuid from "./js/worker-components/uuid.js" // This is used globally, not here though
-
-// Global handler
-function G(){
-	// Declare globals
-	// eslint-disable-next-line no-unused-vars
-	window.internal = {
-		workers: {
-			list: {},
-			components: [
-				// List of files loadable by workers in src/js/worker-components
-				"compress.js",
-				"export.js",
-				"google-drive.js",
-				"hash.js",
-				"onedrive.js",
-				"uuid.js"
-			],
-			handlers: {}
-		},
-		ui: {},
-		console: {
-			list: [
-				// These values will be put behind a wrapper for log collection
-				"log",
-				"error",
-				"info",
-				"trace",
-				"warn"
-			],
-			cache: {},
-			logs: []
-		},
-		time_at_live: new Date().getTime(), // Time since code first started to run
-		bundle: null
-	}
-	// Public libs
-	window.public = {
-		uuid: uuid,
-		Workers: Workers
-	}
-	// Run the console component here
-	C.C()
-}
-
 // Main app function
 function App() {
-	// Declare globals
-	G()
-
-	// Check browser compatibility
-	if(c.c()){
-		console.log("Browser compatibility checks passed!")
-		// Handle workers
-		Workers.default()
-		return (
-			<div>
-				<p>Matikkaeditori.fi, testi 1</p>
-				<p>Kissa koira lammas vladimir</p>
-			</div>
-		)
-	}else {
-		console.log("Incompatible browser!")
-		// TODO: Make a cleaner unsupported message.
-		return (
-			<p>
-				Your browser is incompatible (Supported: Chrome, Firefox, Edge... basically any modern browser, except any version of non-chromium IE).
-				<br></br>
-				Some required components are missing.
-			</p>
-		)
-	}
-	
 	// Return base page
-	//return (
-	//	<div>
-	//		Matikkaeditori.fi, testi 10/02
-	//	</div>
-	//)
+	return (
+		<div>
+			Matikkaeditori.fi v3
+		</div>
+	)
 }
 
 export default App
--- src/index.js
+++ src/index.js
@@ -1,11 +1,79 @@
+// React libs
 import React from "react"
 import ReactDOM from "react-dom"
 import "./css/index.css"
 import App from "./App"
 
-ReactDOM.render(
-	<React.StrictMode>
-		<App></App>
-	</React.StrictMode>,
-	document.getElementById("root")
-)
+// Import static components
+import * as C from "./js/console.js"
+import * as c from "./js/worker-components/compatibility.js"
+import * as Workers from "./js/workers.js"
+// eslint-disable-next-line no-unused-vars
+import * as uuid from "./js/worker-components/uuid.js" // This is used globally, not here though
+
+// Global handler
+function G(){
+	// Declare globals
+	// eslint-disable-next-line no-unused-vars
+	window.internal = {
+		workers: {
+			list: {},
+			components: [
+				// List of files loadable by workers in src/js/worker-components
+				"compress.js",
+				"export.js",
+				"google-drive.js",
+				"hash.js",
+				"onedrive.js",
+				"uuid.js"
+			],
+			handlers: {}
+		},
+		ui: {},
+		console: {
+			list: [
+				// These values will be put behind a wrapper for log collection
+				"log",
+				"error",
+				"info",
+				"trace",
+				"warn"
+			],
+			cache: {},
+			logs: []
+		},
+		time_at_live: new Date().getTime(), // Time since code first started to run
+		bundle: null
+	}
+	// Public libs
+	window.public = {
+		uuid: uuid,
+		Workers: Workers
+	}
+	// Run the console component here
+	C.C()
+}
+
+// DOM renderer
+function Render(){
+	ReactDOM.render(
+		<React.StrictMode>
+			<App></App>
+		</React.StrictMode>,
+		document.getElementById("root")
+	)
+}
+
+// Declare globals
+G()
+
+// Check browser compatibility
+if(c.c()){
+	console.log("Browser compatibility checks passed!")
+	// --- This is where the actual application code begins ---
+	// Handle workers
+	Workers.default()
+	Render()
+}else {
+	console.log("Incompatible browser!")
+}
