2a66ea05d92a35ed2fb01a08f56f5ccdfc83ea8b
Esinko <34211401+Esinko@users.noreply.github.com>
2021-03-28 23:10:28 +0300

    feat: Hashing functionality

--- src/js/worker-components/hash.js
+++ src/js/worker-components/hash.js
@@ -1,1 +1,23 @@
-// Placeholder
+/**
+ * Implement hashing functionality
+ * 
+ * By: @Esinko
+ */
+export default class hash {
+    /**
+     * The input for the hashing algorithm
+     * @param {String} input The string to hash
+     */
+    constructor(input){
+        this.input = input
+    }
+
+    /**
+     * Hash the data with the SHA1 algorithm
+     */
+    async sha1(){
+        if(this._inputIsValid()){
+            return Array.from(new Uint8Array(await crypto.subtle.digest("SHA-1", new TextEncoder("utf-8").encode(this.input)))).map(b => ("00" + b.toString(16)).slice(-2)).join("")
+        } 
+    }
+}
