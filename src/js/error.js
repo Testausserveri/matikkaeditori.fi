067134c2f5abb4e26c0565b7900a3dc043f081ef
Esinko <34211401+Esinko@users.noreply.github.com>
2021-03-29 20:20:52 +0300

    feat: Error is now a single message

--- src/js/error.js
+++ src/js/error.js
@@ -7,7 +7,5 @@
  */
 export default async function error(source, message){
     let css = "color: #d42c2c; font-size: 33px; font-family: Inconsolata, monospace; font-weight: bold;"
-    console.log("%c-------------------------\nError (" + source + "):", css)
-    console.log(message)
-    console.log("%c-------------------------", css)
+    console.log("%c-------------------------\nError (" + source + "):\n\n"+ "%c" + "%s\n" + "%c-------------------------", css, "color: unset;", message, css)
 }
