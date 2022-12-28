a6de8b180e919dde05b424a92717bf430db06e6b
Eldemarkki <38920928+Eldemarkki@users.noreply.github.com>
2022-12-28 03:27:10 +0200

    chore(typescript): convert date.js to typescript

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -1,17 +1,4 @@
-// eslint-disable-next-line no-extend-native
-Date.prototype.getWeek = () => {
-    const baseDate = new Date()
-    const onejan = new Date(
-        baseDate.getFullYear(), 0, 1
-    )
-    const today = new Date(
-        baseDate.getFullYear(), baseDate.getMonth(), baseDate.getDate()
-    )
-    const dayOfYear = ((today - onejan + 86400000) / 86400000)
-    return Math.ceil(dayOfYear / 7)
-}
-
-export default function formatDate(dateInput) {
+export default function formatDate(dateInput: string | number | Date) {
     const date = new Date(dateInput) // To object
     const today = new Date()
     let pretty
@@ -19,7 +6,7 @@ export default function formatDate(dateInput) {
         pretty = "Tänään"
     } else if (today.getDate() - 1 === date.getDate() && today.getMonth() === date.getMonth() && today.getFullYear() === date.getFullYear()) { // This will not work when month changes, but that's minor stuff
         pretty = "Eilen"
-    } else if (today.getWeek() === date.getWeek() && today.getFullYear() === date.getFullYear()) {
+    } else if (today.getFullYear() === date.getFullYear()) { // TODO: Check if this is correct. See the changes that happened in the Typescript pull request (https://github.com/Testausserveri/matikkaeditori.fi-v3/pull/47)
         pretty = ["Sunnuntai", "Maanantai", "Tiistai", "Keskiviikko", "Torstai", "Perjantai", "Lauantai"][date.getDay()]
     } else {
         pretty = `${date.getDate()}.${date.getMonth() + 1}.${date.getFullYear()}`
